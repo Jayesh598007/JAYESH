8fbc2ff8d62a667d5716802fad8809ea7c4973d8
Jayesh <jayeshchau598007@gmail.com>
2022-02-01 20:08:10 +0530

    some files deleted

--- CSS/CSS_JC_practice/button_color/but_col.js
+++
@@ -1,7 +0,0 @@
-let colPress = document.getElementById('press');
-let btnRed = document.getElementById('red');
-let btnGreen = document.getElementById('green');
-
-
-btnRed.addEventListener("click", ()=> colPress.style.backgroundColor="red")
-btnGreen.addEventListener("click", ()=> colPress.style.backgroundColor="green")
--- CSS/CSS_JC_practice/onclick/onclick.js
+++
@@ -1,5 +0,0 @@
-function col(){
-    // document.getElementById('trans').style.backgroundColor = "yellow";
-    document.getElementById('trans').style.backgroundImage= "radial-gradient(red, yellow, green)";
-
-}
